5cf2e3584d8b459803ca5bd54cccfffa4ddef88f
Zachary Neubert <zacneubert@gmail.com>
2016-10-27 11:46:02 -0600

    Made histograms happen

--- plotter.py
+++ plotter.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 
-with open('cppResults.csv', 'r') as infile:
-    cpp = [(0,float(i)) for i in infile.split(',')]
-with open('scalaResults.csv', 'r') as infile:
-    scala = [(1,float(i)) for i in infile.split(',')]
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
 
-# Plot the Results or something
+with open('cpp.csv', 'r') as infile:
+    cpp = [float(i) for i in infile.read().split(',')]
+with open('scala.csv', 'r') as infile:
+    scala = [float(i) for i in infile.read().split(',')]
+
+
+# the histogram of the data
+n, bins, patches = plt.hist(cpp, 50, normed=1, facecolor='green', alpha=0.75)
+n, bins, patches = plt.hist(scala, 50, normed=1, facecolor='red', alpha=0.75)
+
+# add a 'best fit' line
+plt.xlabel('Time')
+plt.ylabel('Frequency')
+plt.title(r'Comparison of scala and CPP')
+#plt.axis([40, 160, 0, 0.03])
+plt.grid(True)
+
+plt.show()
